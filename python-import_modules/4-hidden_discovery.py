29b65d0a1fbcc3faf1414a4efdebe5c80b30fbdf
Avaz Asgarov <avaz.asgarov45@gmail.com>
2026-04-04 22:48:18 +0400

    Fix Task 4: Match ALX/Holberton standard and ensure executable bit

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,6 @@
 import hidden_4
 
 if __name__ == "__main__":
-    names = dir(hidden_4)
-    for name in sorted(names):
-        if not name.startswith("__"):
-            print("{}".format(name))
+    for name in sorted(dir(hidden_4)):
+        if name[:2] != "__":
+            print(name)
